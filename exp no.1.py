7609d3930b076610e62476d3c302da7595bc85ab
Shantanu-2712 <shantanujagtap624@gmail.com>
2026-02-05 11:14:48 +0530

    python programming exp 1

---
+++ exp no.1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  5 10:50:54 2026
+
+@author: User
+"""
+
+name=input("Enter your name: ")
+age=input("Enter your age: ")
+roll_no=input("Enter your roll no:")
+city=input("Enter your city:")
+print("/n----Student Information----")
+print("Name:", name)
+print("Age:", age)
+print("Roll Number:", roll_no)
+print("City:", city)
